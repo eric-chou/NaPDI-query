60bd9f19c5c29f469610cb66c79b19658271b84d
Eric <eeeric.chou@gmail.com>
2018-02-26 13:23:42 -0500

    db names updated for some files

--- database/scripts/load-seed-data.sh
+++ database/scripts/load-seed-data.sh
@@ -4,9 +4,9 @@
 
 echo "[INFO] Begin load seed data..."
 
-psql -U postgres -h localhost -p 5432 -d napdi_repository -a -f ../functions/setval_max.function.sql
-psql -U postgres -h localhost -p 5432 -d napdi_repository -a -f ../seed.sql
-psql -U postgres -h localhost -p 5432 -d napdi_repository -a -f ../test.sql
-psql -U postgres -h localhost -p 5432 -d napdi_repository -a -f ../seed_concept_ids.sql
+psql -U postgres -h localhost -p 5432 -d demo_napdi_repository -a -f ../functions/setval_max.function.sql
+psql -U postgres -h localhost -p 5432 -d demo_napdi_repository -a -f ../seed.sql
+psql -U postgres -h localhost -p 5432 -d demo_napdi_repository -a -f ../test.sql
+psql -U postgres -h localhost -p 5432 -d demo_napdi_repository -a -f ../seed_concept_ids.sql
 
 echo "[INFO] load seed data finished"
